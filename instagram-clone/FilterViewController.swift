3851f438495f0eb371383f1c9ac83229bd41b42c
Minsoo Kwak <marcosdeseul@gmail.com>
2017-03-21 22:03:58 +0800

    Add filter feature

--- instagram-clone/FilterViewController.swift
+++ instagram-clone/FilterViewController.swift
@@ -14,6 +14,22 @@ class FilterViewController: UIViewController, UICollectionViewDelegate, UICollec
     @IBOutlet weak var collectionView: UICollectionView!
     var image: UIImage?
     
+    let filterNameList = [
+        "No Filter",
+        "CIPhotoEffectChrome",
+        "CIPhotoEffectFade",
+        "CIPhotoEffectInstant",
+        "CIPhotoEffectMono",
+    ]
+    let filterDisplayNameList = [
+        "Normal",
+        "Chrome",
+        "Fade",
+        "Instant",
+        "Mono",
+    ]
+    let context = CIContext(options: nil)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         imageView.image = image!
@@ -26,6 +42,27 @@ class FilterViewController: UIViewController, UICollectionViewDelegate, UICollec
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func createFilteredImage(filterName: String, image: UIImage) -> UIImage {
+        // 1 - create source image
+        let sourceImage = CIImage(image: image)
+        
+        // 2 - create filter using name
+        let filter = CIFilter(name: filterName)
+        filter?.setDefaults()
+        
+        // 3 - set source image
+        filter?.setValue(sourceImage, forKey: kCIInputImageKey)
+        
+        // 4 - output filtered image as cgImage with dimension.
+        let outputCGImage = context.createCGImage((filter?.outputImage!)!, from: (filter?.outputImage!.extent)!)
+        
+        // 5 - convert filtered CGImage to UIImage
+        let filteredImage = UIImage(cgImage: outputCGImage!)
+        
+        return filteredImage
+    }
+    
     
     // MARK: UICollectionViewDataSource
     
@@ -43,8 +80,14 @@ class FilterViewController: UIViewController, UICollectionViewDelegate, UICollec
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
 //        let cell = UICollectionViewCell()
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "FilterCell", for: indexPath) as! FilterCell
-        cell.filterImage.image = image
-        cell.filterLabel.text = "filter \(indexPath.row)"
+        
+        var filteredImage = image
+        if indexPath.row != 0 {
+            filteredImage = createFilteredImage(filterName: filterNameList[indexPath.row], image: image!)
+        }
+        
+        cell.filterImage.image = filteredImage
+        cell.filterLabel.text = filterDisplayNameList[indexPath.row]
         
         // Configure the cell
         return cell
